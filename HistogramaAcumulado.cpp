45b972fd2bb9f762e5b3be1769472801d5d17ea6
Pedro José Piquero Plaza <gowikel@gmail.com>
2013-11-06 14:42:59 +0100

    Añadido el metodo HistogramaAcumulado::procesarDatos

--- HistogramaAcumulado.cpp
+++ HistogramaAcumulado.cpp
@@ -1,14 +1,28 @@
 #include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
 #include "Histograma.hpp"
 #include "HistogramaAcumulado.hpp"
 
 using namespace FSIV;
 using namespace std;
+using namespace cv;
 
 namespace FSIV
 {
   void HistogramaAcumulado::procesarDatos(const Mat &m, const Mat &mascara)
   {
+    assert((mascara.data == NULL) || (mascara.size() == m.size()));
+    assert((mascara.data == NULL) || (mascara.type() == m.type()));
+    Histograma::procesarDatos(m, mascara);
+
+    double acumulado = 0;
+
+    for(unsigned int i = 0; i < _histograma.size(); i++)
+      {
+	acumulado += this->getElemento(i);
+	_histograma[i] = acumulado;
+      }
   }
 
   void HistogramaAcumulado::normalizar()
