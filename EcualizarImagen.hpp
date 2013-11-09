09faff2937c4cb094bbb046d19fba3eea6b9a3f4
Pedro José Piquero Plaza <gowikel@gmail.com>
2013-11-09 13:15:51 +0100

    Se ha añadido el espacio de color HSL a la lista de espacios de colores disponibles

--- EcualizarImagen.hpp
+++ EcualizarImagen.hpp
@@ -11,7 +11,7 @@ using namespace cv;
 
 namespace FSIV
 {
-  enum espacioColor { ESPACIO_COLOR_HSV, ESPACIO_COLOR_CIE, ESPACIO_COLOR_YCrCb };
+  enum espacioColor { ESPACIO_COLOR_HSV, ESPACIO_COLOR_CIE, ESPACIO_COLOR_YCrCb, ESPACIO_COLOR_HSL };
 
   /** @brief Clase que realiza la ecualización de una imagen. **/
   class EcualizarImagen
